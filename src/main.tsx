6a05a34cfdd6536a2c2d41e17b001f6c76ecab64
Nikita <nikitakemper555@gmail.com>
2023-06-26 19:30:33 +0300

    feat: add normalize.scss and App component

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import App from './components/App/App';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <div>my App</div>
+    <App />
   </React.StrictMode>
 );
