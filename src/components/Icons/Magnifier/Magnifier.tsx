48f6d45e9829c4ff8bd4a9a763bbfa2faa2f2bec
Nikita <nikitakemper555@gmail.com>
2023-06-27 18:09:06 +0300

    feat: add Header component

---
+++ src/components/Icons/Magnifier/Magnifier.tsx
@@ -0,0 +1,19 @@
+function Magnifier() {
+  return (
+    <svg
+      width="25px"
+      height="25px"
+      xmlns="http://www.w3.org/2000/svg"
+      fill="#7F7F7F"
+      viewBox="0 0 24 24"
+      id="magnifier"
+    >
+      <path
+        d="M18,10a8,8,0,1,0-3.1,6.31l6.4,6.4,1.41-1.41-6.4-6.4A8,8,0,0,0,18,10Zm-8,6a6,6,0,1,1,6-6A6,6,0,0,1,10,16Z"
+        data-name="Layer 2"
+      ></path>
+    </svg>
+  );
+}
+
+export default Magnifier;
