50ca0dda7787aca7ee99542954dc3eb7ed22db0d
Nikita <nikitakemper555@gmail.com>
2023-06-29 00:16:23 +0300

    feat: add searchbar input logic

--- src/components/Searchbar/Searchbar.tsx
+++ src/components/Searchbar/Searchbar.tsx
@@ -1,11 +1,35 @@
 import './Searchbar.scss';
+
 import Magnifier from '../Tools/Magnifier/Magnifier';
+import React, { ChangeEvent, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 function Searchbar() {
+  const [value, setValue] = useState<string>('');
+  const navigate = useNavigate();
+
+  const onChangeClick = (e: ChangeEvent<HTMLInputElement>) => setValue(e.target.value);
+
+  const onEnterClick = (event: React.KeyboardEvent<HTMLInputElement>) => {
+    if (event.key === 'Enter') {
+      navigate(`/search/${value}`, { replace: true });
+    }
+  };
+
   return (
     <div className="input__wrapper">
-      <input className="input" type="text" placeholder="Search for free photos" />
-      <button className="input__button">
+      <input
+        className="input"
+        type="text"
+        value={value}
+        onChange={onChangeClick}
+        placeholder="Search for free photos"
+        onKeyDown={onEnterClick}
+      />
+      <button
+        className="input__button"
+        onClick={() => navigate(`/search/${value}`, { replace: true })}
+      >
         <Magnifier />
       </button>
     </div>
