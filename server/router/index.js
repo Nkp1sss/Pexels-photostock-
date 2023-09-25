525a00b450f17e08cc26e7efc8234c200da3fdea
Nikita <nikitakemper555@gmail.com>
2023-09-25 20:37:30 +0300

    fix: fix cors error

--- server/router/index.js
+++ server/router/index.js
@@ -4,7 +4,6 @@ const router = new Router();
 const { body } = require('express-validator');
 const authMiddleware  = require('../middlewares/auth-middleware');
 
-
 router.post('/registration',
   body('email').isEmail(),
   body('password').isLength({ min: 3, max: 32 }),
